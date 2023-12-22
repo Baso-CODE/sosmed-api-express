77fb4cbc6b8b741e69730c4ab2af3471031cbd01
baso_afriza <134659802+Baso-CODE@users.noreply.github.com>
2023-12-22 15:33:34 +0700

    feat: hashing password (#3)

--- src/actions/users/registerUserAction.ts
+++ src/actions/users/registerUserAction.ts
@@ -3,31 +3,29 @@
 // KALAU ADA LANGSUNG RETURN EMAIL / USERNAME YANG TELAH DI GUNAKAN
 // KALAU TIDAK ADA YANG MENGGUNAKAN EMAIL / USERNAME TERSEBUT CREATE KE DATABASE
 
+import { hashPassword } from "../../helper/bcrypt";
 import { createUser } from "../../repositories/users/createUser";
 import { findUsersByEmailAndUsername } from "../../repositories/users/findUsersByEmailAndUsername";
 import { IUser } from "../../types/user.type";
 
 export const registerUserAction = async (data: IUser) => {
   try {
-    // cari email di databse
-    const { email, username } = data;
+    const { email, username, password } = data;
     const users = await findUsersByEmailAndUsername(email, username);
 
-    //cek email atau username udah ada yang pake tidak
     if (users.length) {
       return {
         status: 400,
         message: "email or username already exists",
       };
     }
-
-    // crate user di database
+    const hashedPassword = await hashPassword(password);
+    data.password = hashedPassword;
     await createUser(data);
     return {
       status: 200,
       message: "User created successfully",
     };
-    console.log(users);
   } catch (error) {
     console.log(error);
     throw error;
